6b264aa8925a1597e1529f3986c7bfa1cc4c0f6c
arimora13 <amoraobando@gmail.com>
2026-06-02 18:28:01 -0600

    Plantilla visual trabajo 1

---
+++ src/main/java/cr/huertitas/HuerTicasApplication.java
@@ -0,0 +1,11 @@
+package cr.huertitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuerTicasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HuerTicasApplication.class, args);
+    }
+}
