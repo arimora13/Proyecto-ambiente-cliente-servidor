7018d5a8375aea77ad37d327a07c8c3901e26729
Joselito-07 <joelito.orozmenez07@gmail.com>
2026-08-16 16:03:52 -0600

    Alertas cambios

--- alertas/editar.php
+++ alertas/editar.php
@@ -7,20 +7,28 @@ require_once("../clases/Alerta.php");
 $alertaObj = new Alerta($conexion);
 
 //inicio de secuencia if para validaciones
-if (!isset($_GET['id'])) { //si no existe entra al if
+if (!isset($_GET['id']) || empty($_GET['id'])) { //si no existe entra al if
     header("Location: listar.php");
     //regresa al listado
     exit;
 }
 
 //si el formulario se envio
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $alertaObj->editar($_POST['id'], $_POST['idEstado'], $_POST['descripcion']);
+$idAlerta = $_GET['id'];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $idEstado = $_POST['idEstado'] ?? null;
+    $descripcion = $_POST['descripcion'] ?? '';
+    $idForm = $_POST['id'] ?? $idAlerta;
+    
+    $alertaObj->editar($idForm, $idEstado, $descripcion);
+
     //actualiza la info y redirige al listado
     header("Location: listar.php?msg=actualizada");
+    exit;
 }
 //trae los datos de la alerta seleccionada
-$datos = $alertaObj->obtenerPorId($_GET['id']);
+$datos = $alertaObj->obtenerPorId($idAlerta);
 
 //si esta no existe
 if (!$datos) {
