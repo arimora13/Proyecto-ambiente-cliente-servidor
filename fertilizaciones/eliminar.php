1cb43ba214810267f477a9f0c6c12a522ece421a
Joselito-07 <joelito.orozmenez07@gmail.com>
2026-08-16 17:43:54 -0600

    Ferti Eliminar cambio

--- fertilizaciones/eliminar.php
+++ fertilizaciones/eliminar.php
@@ -1,12 +1,18 @@
 <?php
 require_once("../conexion.php");
 validarSesion();
+validarRol(["Administrador","Docente"]);
+
 require_once("../clases/Fertilizacion.php");
 
-if (isset($_GET['id'])) {
+if (isset($_GET['id']) && !empty($_GET['id'])) {
     $fertilizacionObj = new Fertilizacion($conexion);
     $fertilizacionObj->eliminar($_GET['id']);
 }
 header("Location: listar.php?msg=eliminado");
+exit;
+
+//Si se mete un id valio se envia de vuelta al listado
+header("Location: listar.php");
 exit;
 ?>
