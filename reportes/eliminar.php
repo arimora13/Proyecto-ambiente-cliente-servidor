75afde6526021348f71300c404bd4771dbdb35e2
Joselito-07 <joelito.orozmenez07@gmail.com>
2026-08-16 21:22:20 -0600

    reportes eliminar cambios

--- reportes/eliminar.php
+++ reportes/eliminar.php
@@ -6,13 +6,15 @@ validarRol(["Administrador","Docente"]); //validar el rol
 require_once("../clases/Reporte.php");
 
 //en caso de encontrar el id ingresado 
-if(isset($_GET['id'])){
+if(isset($_GET['id']) && !empty($_GET['id'])){
     $reporteObj = new Reporte($conexion);
     $reporteObj->eliminar($_GET['id']); //se llama al metodo de eliminar
+    //se muestra mensaje exitoso
+    header("Location: listar.php?msg=eliminado");
+    exit;
+} else {
+    header("Location: listar.php");
+    exit;
 }
-
-//se muestra mensaje exitoso
-header("Location: listar.php?msg=eliminado");
-exit;
 
 ?>
