2feaf7ea46e0ef151461d375de65e0551eebc22d
Joselito-07 <joelito.orozmenez07@gmail.com>
2026-08-16 22:47:42 -0600

    index cambios

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 require_once("conexion.php");
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 if (isset($_SESSION['id_usuario'])) {
     header("Location: menu.php");
 } else {
