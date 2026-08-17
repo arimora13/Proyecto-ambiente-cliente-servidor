01cd6d5cceb0334cd8a631e25eefebce9bacdac7
Joselito-07 <joelito.orozmenez07@gmail.com>
2026-08-16 23:12:17 -0600

    cambios registrar usuario

--- usuarios/registrar.php
+++ usuarios/registrar.php
@@ -3,7 +3,8 @@ require_once("../conexion.php");
 validarSesion();
 validarRol(["Administrador"]);
 
-$listaRoles   = $conexion->query("SELECT ID_ROL, NOMBRE_ROL FROM ROL")->fetchAll(PDO::FETCH_ASSOC);
+$listaRoles   = $conexion->query("SELECT ID_ROL, NOMBRE_ROL FROM ROL ORDER BY NOMBRE_ROL")->fetchAll(PDO::FETCH_ASSOC);
 $listaEstados = $conexion->query("SELECT ID_ESTADO, NOMBRE_ESTADO FROM ESTADO")->fetchAll(PDO::FETCH_ASSOC);
 
 include(__DIR__ . "/vistas/registrar_vista.html");
+?>
