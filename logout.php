7e81480dce8f2b213458cb8e4403bde7415c309d
Joselito-07 <joelito.orozmenez07@gmail.com>
2026-08-16 22:50:42 -0600

    logout cambios

--- logout.php
+++ logout.php
@@ -1,7 +1,14 @@
 <?php
 require_once("conexion.php");
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+$_SESSION = array();
 session_unset();
 session_destroy();
+
 header("Location: login.php");
 exit;
 ?>
