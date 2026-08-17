665dcc28dd5b722a1c41e45e35e202e23d73642e
Joselito-07 <joelito.orozmenez07@gmail.com>
2026-08-16 22:39:26 -0600

    eliminar cultivos cambios

--- tipos_cultivo/eliminar.php
+++ tipos_cultivo/eliminar.php
@@ -3,10 +3,13 @@ require_once("../conexion.php");
 validarSesion();
 require_once("../clases/TipoCultivo.php");
 
-if (isset($_GET['id'])) {
+if (isset($_GET['id']) || !empty($_GET['id'])) {
     $tipoObj = new TipoCultivo($conexion);
     $tipoObj->eliminar($_GET['id']);
+    header("Location: listar.php?msg=eliminado");
+    exit;
+} else {
+    header("Location: listar.php");
+    exit;
 }
-header("Location: listar.php?msg=eliminado");
-exit;
 ?>
