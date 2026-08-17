caf3ef376c2fa515a52481edfa35a80f2eb01317
Joselito-07 <joelito.orozmenez07@gmail.com>
2026-08-16 21:12:16 -0600

    reportes listar cambios

--- reportes/listar.php
+++ reportes/listar.php
@@ -4,8 +4,10 @@ validarSesion(); //sesion validada
 validarRol(["Administrador","Docente"]); //validacion
 require_once("../clases/Reporte.php"); // cargar clase
 
-$reporteObj =new Reporte($conexion); //generacion del objeto
+$reporteObj = new Reporte($conexion); //generacion del objeto
+
 $lista = $reporteObj->listar();  //llamar a listar
 
 //mostrar
 include(__DIR__ . "/vistas/listar_vista.html");
+?>
